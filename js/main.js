bd2a2797e0b03ed85d2a643f21af9f665948015d
linuxmobile <bdiez19@gmail.com>
2022-12-02 14:31:17 -0300

    Feat: added dom elements

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// █▀▄ █▀█ █▀▄▀█   █▀▀ █░░ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀ █▀
+// █▄▀ █▄█ █░▀░█   ██▄ █▄▄ ██▄ █░▀░█ ██▄ █░▀█ ░█░ ▄█
+const products = document.querySelector(".products-container");
+const productsCart = document.querySelector(".cart-container");
+const total = document.querySelector(".total");
+const categories = document.querySelector(".categories");
+const categoriesList = document.querySelectorAll(".category");
+const btnLoad = document.querySelector(".btn-load");
+const buyBtn = document.querySelector(".btn-buy");
+// const cartBubble = document.querySelector(".cart-bubble");
+// const cartBtn = document.querySelector(".cart-label");
+// const barsBtn = document.querySelector(".menu-label");
+// const cartMenu = document.querySelector(".cart");
+// const barsMenu = document.querySelector(".navbar-list");
+// const overlay = document.querySelector(".overlay");
+// const successModal = document.querySelector(".add-modal");
+// const deleteBtn = document.querySelector(".btn-delete");
