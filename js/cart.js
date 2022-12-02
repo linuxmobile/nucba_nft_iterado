4dd5b67a9abe08aee4dfa674599a679f686916a3
linuxmobile <bdiez19@gmail.com>
2022-12-02 14:32:29 -0300

    Feat: added toggle menu logic

--- js/cart.js
+++ js/cart.js
@@ -17,3 +17,46 @@ const barsMenu = document.querySelector(".navbar-list");
 const overlay = document.querySelector(".overlay");
 const successModal = document.querySelector(".add-modal");
 const deleteBtn = document.querySelector(".btn-delete");
+
+
+// █▀▄▀█ █▀▀ █▄░█ █░█   █░░ █▀█ █▀▀ █ █▀▀
+// █░▀░█ ██▄ █░▀█ █▄█   █▄▄ █▄█ █▄█ █ █▄▄
+
+// creamos funcion toggleMenu y toggleCart para mostrar y ocultar los menus
+const toggleMenu = () => {
+    barsMenu.classList.toggle("open-menu");
+    if (cartMenu.classList.contains("open-cart")) {
+      cartMenu.classList.remove("open-cart");
+      return; // Si ya había algo abierto, no se togglea el overlay, por eso el return
+    }
+    overlay.classList.toggle("show-overlay");
+  };
+  
+const toggleCart = () => {
+    cartMenu.classList.toggle("open-cart");
+    if (barsMenu.classList.contains("open-menu")) {
+        barsMenu.classList.remove("open-menu");
+        return; // Si ya había algo abierto, no se togglea el overlay, por eso el return
+    }
+    overlay.classList.toggle("show-overlay");
+};
+
+// creamos la funcion para cerrar el menu y el carrito al hacer click en el overlay
+const closeOnOverlayClick = () => {
+    barsMenu.classList.remove("open-menu");
+    cartMenu.classList.remove("open-cart");
+    overlay.classList.remove("show-overlay");
+};
+
+
+// █ █▄░█ █ ▀█▀ █ ▄▀█ █░░ █ ▀█ █▀▀ █▀█
+// █ █░▀█ █ ░█░ █ █▀█ █▄▄ █ █▄ ██▄ █▀▄
+// creamos la funcion para mostrar el menu y el carrito
+const clickEvent = () => {
+    cartBtn.addEventListener("click", toggleCart);
+    barsBtn.addEventListener("click", toggleMenu);
+    overlay.addEventListener("click", closeOnOverlayClick);
+  }
+
+// ejecutamos la funcion clickEvent
+clickEvent();
