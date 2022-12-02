d230cfe2aa229981dcf47cfc750841a4ee97389b
linuxmobile <bdiez19@gmail.com>
2022-12-02 14:31:42 -0300

    Feat: added render products

--- js/main.js
+++ js/main.js
@@ -15,3 +15,99 @@ const buyBtn = document.querySelector(".btn-buy");
 // const overlay = document.querySelector(".overlay");
 // const successModal = document.querySelector(".add-modal");
 // const deleteBtn = document.querySelector(".btn-delete");
+
+
+// █▀█ █▀▀ █▄░█ █▀▄ █▀▀ █▀█   █▀█ █▀█ █▀█ █▀▄ █░█ █▀▀ ▀█▀ █▀
+// █▀▄ ██▄ █░▀█ █▄▀ ██▄ █▀▄   █▀▀ █▀▄ █▄█ █▄▀ █▄█ █▄▄ ░█░ ▄█
+
+// utilizamos fetch para traer los datos del data.json
+const getData = async () => {
+    const response = await fetch("./js/data.json");
+    const data = await response.json();
+    return data;
+}
+
+// creamos una función para mostrar los productos en el dom
+const renderProduct = (product) => {
+    const {id, name, bid, user, userImg, cardImg} = product;
+    return `
+    <div class="product">
+        <img src=${cardImg} alt=${name} />
+        <div class="product-info">
+            <!-- top -->
+            <div class="product-top">
+                <h3>${name}</h3>
+                <p>Current Bid</p>
+            </div>
+            <!-- mid -->
+            <div class="product-mid">
+                <div class="product-user">
+                    <img src=${userImg} alt="user" />
+                    <p>@${user}</p>
+                </div>
+                <span>${bid} eTH</span>
+            </div>
+            <!-- bot -->
+            <div class="product-bot">
+                <div class="product-offer">
+                    <div class="offer-time">
+                        <img src="./assets/img/fire.png" alt="" />
+                        <p>05:12:07</p>
+                    </div>
+                    <button class="btn-add"
+                    data-id='${id}'
+                    data-name='${name}'
+                    data-bid='${bid}'
+                    data-img='${cardImg}'>Add</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
+}
+
+// para la paginacion del ver mas
+// tomamos el array de productos del data.json y lo dividimos usando "size"
+// en este caso 6 elementos del data.json
+const splitProducts = (products, size) => {
+    const dividedProducts = [];
+    for (let i = 0; i < products.length; i += size) {
+        dividedProducts.push(products.slice(i, i + size));
+    }
+    return dividedProducts;
+}
+
+// funcion para dividir los productos del data.json de 6 en 6 y manejar la paginacion
+const productsController = async () => {
+    const data = await getData();
+    const dividedProducts = splitProducts(data, 6);
+    let currentPage = 0;
+    const showProducts = () => {
+        const productsToShow = dividedProducts[currentPage];
+        productsToShow.forEach(product => {
+            products.innerHTML += renderProduct(product);
+        });
+    }
+    showProducts();
+    btnLoad.addEventListener("click", () => {
+        currentPage++;
+        if (currentPage < dividedProducts.length) {
+            showProducts();
+        } else {
+            btnLoad.style.display = "none";
+        }
+    });
+    console.log (dividedProducts);
+}
+
+
+
+
+// █ █▄░█ █ ▀█▀ █ ▄▀█ █░░ █ ▀█ █▀▀ █▀█
+// █ █░▀█ █ ░█░ █ █▀█ █▄▄ █ █▄ ██▄ █▀▄
+// creamos la funcion init para mostrar los productos en el dom
+const init = () => {
+    productsController();
+}
+
+// llamamos al init
+init();
